d5f636a03e93624d3e949a16225ab0293e73e90b
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-04 15:50:03 -0400

    Update tablesetup.php

--- public_html/SampleRegLogin/tablesetup.php
+++ public_html/SampleRegLogin/tablesetup.php
@@ -7,12 +7,11 @@ try{
 	$stmt = $db->prepare("CREATE TABLE `Users` (
 				`id` int auto_increment not null,
 				`email` varchar(100) not null unique,
-				`first_name` varchar(100)'
+				`first_name` varchar(100),
 				`last_name` varchar(100),
 				`password` varchar(60),
 				`created` timestamp default current_timestamp,
-				`modified` timestamp default current_timestamp on update
-current_timestamp,
+				`modified` timestamp default current_timestamp on update current_timestamp,
 				PRIMARY KEY (`id`)
 				) CHARACTER SET utf8 COLLATE utf8_general_ci");
 	$r = $stmt->execute();
