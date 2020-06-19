471a9a95645668db93e73fe17b3aadecb96537cc
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-19 19:54:38 -0400

    Update CreateAccountTable.sql

--- public_html/SQLTables/CreateAccountTable.sql
+++ public_html/SQLTables/CreateAccountTable.sql
@@ -2,7 +2,6 @@ CREATE TABLE Accounts(
     id int auto_increment,
     account_number varchar(12) NOT NULL,
     user_id int,
-    Date_Of_Birth int,
     account_type varchar(20),
     opened_date DATETIME default CURRENT_TIMESTAMP
     balance decimal(12,2) default 0.00,
