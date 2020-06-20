51dfd9d48f7186c8d747dc833fd615e4b0cb7282
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-19 20:23:47 -0400

    Delete create_table_accounts.sql

--- public_html/SampleCreateEdit/create_table_accounts.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Accounts (
-    id int auto_increment,
-    name varchar(20) unique,
-    DateOfBirth int default 0,
-    opened_account datetime default current_timestamp,
-    closed_accoount datetime default current_timestamp on update current_timestamp,
-    primary key (id)
-)
