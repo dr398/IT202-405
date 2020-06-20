83a1abb7c6297118c8438785903ad8dc755b56e4
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-20 18:13:33 -0400

    Update delete.php

--- public_html/Week5HW/delete.php
+++ public_html/Week5HW/delete.php
@@ -2,6 +2,6 @@
 require("common.inc.php");
 $db = getDB();
 //example usage, change/move as needed
-$stmt = $db->prepare("SELECT * FROM Things");
+$stmt = $db->prepare("SELECT * FROM Accounts");
 $stmt->execute();
 ?>
