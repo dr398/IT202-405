4b67433ef838aa86be75af2e6af13f118107a6b4
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-19 20:22:27 -0400

    Update create_table_accounts.sql

--- public_html/SampleCreateEdit/create_table_accounts.sql
+++ public_html/SampleCreateEdit/create_table_accounts.sql
@@ -1,9 +1,8 @@
 CREATE TABLE Accounts (
     id int auto_increment,
-    account_number varchar(12) NOT NULL,
-    user_id int,
-    account_type varchar(20),
-    opened_date DATETIME default CURRENT_TIMESTAMP
-    balance decimal(12,2) default 0.00,
+    name varchar(20) unique,
+    DateOfBirth int default 0,
+    opened_account datetime default current_timestamp,
+    closed_accoount datetime default current_timestamp on update current_timestamp,
     primary key (id)
 )
