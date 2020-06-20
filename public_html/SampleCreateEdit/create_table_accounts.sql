c9ecef0f6e51866b7531a60e9e0ecd351e6f3a82
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-19 20:03:43 -0400

    Update and rename create_table_things.sql to create_table_accounts.sql

---
+++ public_html/SampleCreateEdit/create_table_accounts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Accounts (
+    id int auto_increment,
+    account_number varchar(12) NOT NULL,
+    user_id int,
+    account_type varchar(20),
+    opened_date DATETIME default CURRENT_TIMESTAMP
+    balance decimal(12,2) default 0.00,
+    primary key (id)
+)
