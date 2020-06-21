5d10c3d13d0ebdc95e345e8ded17099611c219a0
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-21 16:16:07 -0400

    Update create.php

--- public_html/Week5HW/create.php
+++ public_html/Week5HW/create.php
@@ -10,8 +10,8 @@
 
 <?php
 if(isset($_POST['created'])){
-    $product = $_POST['email'];
-    $price = $_POST['deposit'];
+    $email = $_POST['email'];
+    $deposit = $_POST['deposit'];
     if(!empty($email) && !empty($deposit)){
         require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
