fc4b0ee05963f3718d160ed1b6cbf6aca20bacbf
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-21 16:42:03 -0400

    Update create_table_accounts.sql

--- public_html/create_table_accounts.sql
+++ public_html/create_table_accounts.sql
@@ -1,9 +1,8 @@
-CREATE TABLE Accounts(
+CREATE TABLE Accounts (
     id int auto_increment,
     name varchar(20) unique,
-    balance int default  0,
-    deposit decimal(10,2) default  0.00,
-    modified datetime default current_timestamp  on update current_timestamp ,
-    created datetime default  current_timestamp,
+    deposit int default 0,
+    created datetime default current_timestamp,
+    modified datetime default current_timestamp on update current_timestamp,
     primary key (id)
 )
