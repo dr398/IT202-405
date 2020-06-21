a389a5933b2e2e2e96917c55c4caee278a033bf9
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-21 15:22:17 -0400

    Update create_table_accounts.sql

--- public_html/create_table_accounts.sql
+++ public_html/create_table_accounts.sql
@@ -3,7 +3,6 @@ CREATE TABLE Accounts(
     email varchar(60) NOT NULL unique,
     balance int default  0,
     deposit decimal(10,2) default  0.00,
-    description TEXT,
     modified datetime default current_timestamp  on update current_timestamp ,
     created datetime default  current_timestamp,
     primary key (id),
