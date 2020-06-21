560af839d29607616309334d8b07539dff979aea
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-21 18:05:31 -0400

    Update AccountsTable.sql

--- public_html/Week5HW/AccountsTable.sql
+++ public_html/Week5HW/AccountsTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Accounts (
     id int auto_increment,
     name varchar(20) unique,
+    balance decimal(12,2) default 0.00,
     deposit int default 0,
     created datetime default current_timestamp,
     modified datetime default current_timestamp on update current_timestamp,
