a64146e3017cc743cd6a91f9f602e02c11564b3e
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-25 16:58:11 -0400

    Update delete.php

--- public_html/Week6/delete.php
+++ public_html/Week6/delete.php
@@ -1,12 +1,12 @@
 <?php
-if (isset($_GET["thingId"]) && !empty($_GET["thingId"])){
-    if(is_numeric($_GET["thingId"])){
-        $thingId = (int)$_GET["thingId"];
-        $query = file_get_contents(__DIR__ . "/queries/DELETE_ONE_TABLE_THINGS.sql");
+if (isset($_GET["accountId"]) && !empty($_GET["accountId"])){
+    if(is_numeric($_GET["accountId"])){
+        $accountId = (int)$_GET["accountId"];
+        $query = file_get_contents(__DIR__ . "/queries/Delete_one_table_Accounts.sql");
         if(isset($query) && !empty($query)) {
             require("common.inc.php");
             $stmt = getDB()->prepare($query);
-            $stmt->execute([":id"=>$thingId]);
+            $stmt->execute([":id"=>$accountId]);
             $e = $stmt->errorInfo();
             if($e[0] == "00000"){
                 //we're just going to redirect back to the list
@@ -21,5 +21,5 @@ if (isset($_GET["thingId"]) && !empty($_GET["thingId"])){
     }
 }
 else{
-    echo "Invalid thing to delete";
+    echo "Invalid account to delete";
 }
