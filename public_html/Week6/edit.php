b59b564ee14a70d3e88742b7ee13f18f6111c161
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-25 16:55:21 -0400

    Update edit.php

--- public_html/Week6/edit.php
+++ public_html/Week6/edit.php
@@ -1,7 +1,7 @@
 <?php
-$thingId = -1;
-if(isset($_GET["thingId"]) && !empty($_GET["thingId"])){
-    $thingId = $_GET["thingId"];
+$accountId = -1;
+if(isset($_GET["accountId"]) && !empty($_GET["accountId"])){
+    $accountId = $_GET["accountId"];
 }
 $result = array();
 require("common.inc.php");
@@ -9,26 +9,26 @@ require("common.inc.php");
 <?php
 if(isset($_POST["updated"])){
     $name = "";
-    $quantity = -1;
+    $balance = -1;
     if(isset($_POST["name"]) && !empty($_POST["name"])){
         $name = $_POST["name"];
     }
-    if(isset($_POST["quantity"]) && !empty($_POST["quantity"])){
-        if(is_numeric($_POST["quantity"])){
-            $quantity = (int)$_POST["quantity"];
+    if(isset($_POST["balance"]) && !empty($_POST["balance"])){
+        if(is_numeric($_POST["balance"])){
+            $quantity = (int)$_POST["balance"];
         }
     }
-    if(!empty($name) && $quantity > -1){
+    if(!empty($name) && $balance > -1){
         try{
             $query = NULL;
-            echo "[Quantity" . $quantity . "]";
-            $query = file_get_contents(__DIR__ . "/queries/UPDATE_TABLE_THINGS.sql");
+            echo "[Balance" . $balance . "]";
+            $query = file_get_contents(__DIR__ . "/queries/Update_table_Accounts.sql");
             if(isset($query) && !empty($query)) {
                 $stmt = getDB()->prepare($query);
                 $result = $stmt->execute(array(
                     ":name" => $name,
-                    ":quantity" => $quantity,
-                    ":id" => $thingId
+                    ":balance" => $balance,
+                    ":id" => $balanceId
                 ));
                 $e = $stmt->errorInfo();
                 if ($e[0] != "00000") {
@@ -42,7 +42,7 @@ if(isset($_POST["updated"])){
                 }
             }
             else{
-                echo "Failed to find UPDATE_TABLE_THINGS.sql file";
+                echo "Failed to find Update_table_Accounts.sql file";
             }
         }
         catch (Exception $e){
@@ -50,7 +50,7 @@ if(isset($_POST["updated"])){
         }
     }
     else{
-        echo "Name and quantity must not be empty.";
+        echo "Name and balance must not be empty.";
     }
 }
 ?>
@@ -59,12 +59,12 @@ if(isset($_POST["updated"])){
 //moved the content down here so it pulls the update from the table without having to refresh the page or redirect
 //now my success message appears above the form so I'd have to further restructure my code to get the desired output/layout
 if($thingId > -1){
-    $query = file_get_contents(__DIR__ . "/queries/SELECT_ONE_TABLE_THINGS.sql");
+    $query = file_get_contents(__DIR__ . "/queries/Select_one_table_Accounts.sql");
     if(isset($query) && !empty($query)) {
         //Note: SQL File contains a "LIMIT 1" although it's not necessary since ID should be unique (i.e., one record)
         try {
             $stmt = getDB()->prepare($query);
-            $stmt->execute([":id" => $thingId]);
+            $stmt->execute([":id" => $accountId]);
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
         }
         catch (Exception $e){
@@ -72,24 +72,24 @@ if($thingId > -1){
         }
     }
     else{
-        echo "Failed to find SELECT_ONE_TABLE_THINGS.sql file";
+        echo "Failed to find Select_one_table_Accounts.sql file";
     }
 }
 else{
-    echo "No thingId provided in url, don't forget this or sample won't work.";
+    echo "No accountId provided in url, don't forget this or sample won't work.";
 }
 ?>
 <script src="js/script.js"></script>
 <!-- note although <script> tag "can" be self terminating some browsers require the
 full closing tag-->
 <form method="POST"onsubmit="return validate(this);">
-<label for="thing">Thing Name
+<label for="account">Account Name
     <!-- since the last assignment we added a required attribute to the form elements-->
-    <input type="text" id="thing" name="name" value="<?php echo get($result, "name");?>" required />
+    <input type="text" id="account" name="name" value="<?php echo get($result, "name");?>" required />
 </label>
-<label for="q">Quantity
+<label for="b">Balance
     <!-- We also added a minimum value for our number field-->
-    <input type="number" id="q" name="quantity" value="<?php echo get($result, "quantity");?>" required min="0"/>
+    <input type="number" id="b" name="balance" value="<?php echo get($result, "balance");?>" required min="0"/>
 </label>
-<input type="submit" name="updated" value="Update Thing"/>
+<input type="submit" name="updated" value="Update Account"/>
 </form>
