8e6f997a8fdf4f642d6d694c72e4a711f08d7df4
dr398 <65628616+dr398@users.noreply.github.com>
2020-06-26 12:21:22 -0400

    Update search.php

--- public_html/Week6/search.php
+++ public_html/Week6/search.php
@@ -5,7 +5,7 @@ if(isset($_POST["search"])){
 }
 ?>
 <form method="POST">
-    <input type="text" name="search" placeholder="Search for Account,Descending"
+    <input type="text" name="search" placeholder="Search for Account"
     value="<?php echo $search;?>"/>
     <input type="submit" value="Search"/>
 </form>
