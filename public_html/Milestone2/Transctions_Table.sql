a4b280befd8068449ff816b22bdb7aba88b63fb2
dr398 <65628616+dr398@users.noreply.github.com>
2020-07-02 13:29:51 -0400

    Update Transctions_Table.sql

--- public_html/Milestone2/Transctions_Table.sql
+++ public_html/Milestone2/Transctions_Table.sql
@@ -1,9 +1,12 @@
-CREATE TABLE Transactions (
-    id int auto_increment,
-    name varchar(20) unique,
-    total decimal(12,2) default 0.00,
-    transaction decimal(12,2) default 0.00,
-    created datetime default current_timestamp,
-    modified datetime default current_timestamp on update current_timestamp,
-    primary key (id)
-)
+$query = "create table if not exists `Transactions`(
+        `AccountSource`   not null unique,
+        `AccountDest` varchar(30) not null unique,
+        `Amount` varchar(60) default 0,
+        `Type` int not null default 0,
+	`Total` int not null default 0,
+	PRIMARY KEY (`Id`)
+        ) CHARACTER SET utf8 COLLATE utf8_general_ci";
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+$stmt = $db->prepare($query);
+print_r($stmt->errorInfo());
+$r = $stmt->execute();
