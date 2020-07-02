2d8c75f6db4f9d57b8dc1a6c4887c54c6fede22a
dr398 <65628616+dr398@users.noreply.github.com>
2020-07-02 12:37:43 -0400

    Update Transctions_Table.sql

--- public_html/Milestone2/Transctions_Table.sql
+++ public_html/Milestone2/Transctions_Table.sql
@@ -1,6 +1,5 @@
 CREATE TABLE Transactions (
-    id int auto_increment,
-    name varchar(20) unique,
+    memo varchar(20) unique,
     transaction decimal(12,2) default 0.00,
     created datetime default current_timestamp,
     modified datetime default current_timestamp on update current_timestamp,
