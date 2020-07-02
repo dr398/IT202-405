d0d2e8d33c18de8fa55d0dd20cefc1f3e54d4583
dr398 <65628616+dr398@users.noreply.github.com>
2020-07-02 12:28:40 -0400

    Update and rename Transctions_Table.sql.txt to Transctions_Table.sql

--- public_html/Milestone2/Transctions_Table.sql
+++ public_html/Milestone2/Transctions_Table.sql
@@ -1,7 +1,7 @@
-CREATE TABLE Accounts (
+CREATE TABLE Transactions (
     id int auto_increment,
     name varchar(20) unique,
-    balance decimal(12,2) default 0.00,
+    transaction decimal(12,2) default 0.00,
     created datetime default current_timestamp,
     modified datetime default current_timestamp on update current_timestamp,
     primary key (id)
