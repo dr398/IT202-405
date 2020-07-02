bb9bfc00d5c8b0c894797d073fbffa296694ea32
dr398 <65628616+dr398@users.noreply.github.com>
2020-07-02 14:53:38 -0400

    Update Transctions_Table.sql

--- public_html/Milestone2/Transctions_Table.sql
+++ public_html/Milestone2/Transctions_Table.sql
@@ -2,11 +2,11 @@ CREATE TABLE Transactions (
     id int auto_increment,
     AccountSource int not null,
     AccountDest int (30) not null,
-    Type varchar not null default 0,
+    Type int not null default 0,
     Total int not null default 0,
     created datetime default current_timestamp,
     modified datetime default current_timestamp on update current_timestamp,
-    primary key (id)
-    foreign key (AccountSource)
-    foreign key (AccountDest)
+    primary key (id),
+    foreign key (AccountSource),
+    foreign key (AccountDest),
 )
