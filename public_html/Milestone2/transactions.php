5b3fc46aa628a312476ce250548bc6104f86e168
dr398 <65628616+dr398@users.noreply.github.com>
2020-07-03 10:43:56 -0400

    Update transactions.php

--- public_html/Milestone2/transactions.php
+++ public_html/Milestone2/transactions.php
@@ -2,22 +2,28 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-function do_bank_action($AccountSource, $AccountDest){
+function do_bank_action($account1, $account2, $amountChange, $type){
 	require("config.php");
 	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 	$db = new PDO($conn_string, $username, $password);
 	$a1total = 0;//TODO get total of account 1
 	$a2total = 0;//TODO get total of account 2
-	$query = "INSERT INTO `Transactions` (`AccountSource`, `AccountDest`) 
-	VALUES(:p1acctsrc, :p1acctdest), 
-			(:p2acctsrc, :p2acctdest)";
+	$query = "INSERT INTO `Transactions` (`AccountSource`, `AccountDest`, `Amount`, `Type`, `Total`) 
+	VALUES(:p1a1, :p1a2, :p1change, :type, :a1total), 
+			(:p2a1, :p2a2, :p2change, :type, :a2total)";
 	
 	$stmt = $db->prepare($query);
-	$stmt->bindValue(":p1acctsrc", $AccountSource);
-	$stmt->bindValue(":p1acctdest", $AccountDest);
+	$stmt->bindValue(":p1a1", $account1);
+	$stmt->bindValue(":p1a2", $account2);
+	$stmt->bindValue(":p1change", $amountChange);
+	$stmt->bindValue(":type", $type);
+	$stmt->bindValue(":a1total", $a1total);
 	//flip data for other half of transaction
-	$stmt->bindValue(":p2acctsrc", $AccountDest);
-	$stmt->bindValue(":p2cctdest", $AccountSource);
+	$stmt->bindValue(":p2a1", $account2);
+	$stmt->bindValue(":p2a2", $account1);
+	$stmt->bindValue(":p2change", ($amountChange*-1));
+	$stmt->bindValue(":type", $type);
+	$stmt->bindValue(":a2total", $a2total);
 	$result = $stmt->execute();
 	echo var_export($result, true);
 	echo var_export($stmt->errorInfo(), true);
@@ -25,9 +31,10 @@ function do_bank_action($AccountSource, $AccountDest){
 }
 ?>
 <form method="POST">
-	<input type="text" name="AccountSource" placeholder="Account Number">
+	<input type="text" name="account1" placeholder="Account Number">
 	<!-- If our sample is a transfer show other account field-->
-	<input type="text" name="AccountDest" placeholder="Other Account Number">
+	<?php if($_GET['type'] == 'transfer') : ?>
+	<input type="text" name="account2" placeholder="Other Account Number">
 	<?php endif; ?>
 	
 	<input type="number" name="amount" placeholder="$0.00"/>
@@ -38,7 +45,7 @@ function do_bank_action($AccountSource, $AccountDest){
 </form>
 
 <?php
-if(isset($_POST['type']) && isset($_POST['AccountSource']) && isset($_POST['amount'])){
+if(isset($_POST['type']) && isset($_POST['account1']) && isset($_POST['amount'])){
 	$type = $_POST['type'];
 	$amount = (int)$_POST['amount'];
 	switch($type){
@@ -46,7 +53,7 @@ if(isset($_POST['type']) && isset($_POST['AccountSource']) && isset($_POST['amou
 			do_bank_action("000000000000", $_POST['account1'], ($amount * -1), $type);
 			break;
 		case 'withdraw':
-			do_bank_action($_POST['AccountSource'], "000000000000", ($amount * -1), $type);
+			do_bank_action($_POST['account1'], "000000000000", ($amount * -1), $type);
 			break;
 		case 'transfer':
 			//TODO figure it out
