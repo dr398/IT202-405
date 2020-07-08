a7c2013e5d0d0ce7f70c7df29ccebbabb0eeeadb
dr398 <65628616+dr398@users.noreply.github.com>
2020-07-08 11:36:49 -0400

    Update create.php

--- public_html/Milestone2/create.php
+++ public_html/Milestone2/create.php
@@ -11,21 +11,7 @@ full closing tag-->
     <input type="submit" name="created" value="Create Account"/>
 </form>
 <?php
-require("common.inc.php");
-//find the max id in the table
-$query = "SELECT MAX(id) as max from Accounts";
-$stmt = getDB()->prepare($query);
-$stmt->execute();
-$r = $stmt->fetch(PDO::FETCH_ASSOC);
-$max = (int)$r["max"];//should really check that this value is given correctly, I'm unsafely using it
-$max += 1;//increment by 1 (since this should be the new id that'll get automatically generated
-//pad the number with 0s to the left (this will fit the requirement and be unique since it's based on id
-$account_number = str_pad($str,12,"0",STR_PAD_LEFT);//read it https://www.w3schools.com/php/func_string_str_pad.asp
-//insert the new account number and associate it with the logged in user
-$query = "INSERT INTO Accounts(account_number, id) VALUES(:an, :id)";
-$stmt = getDB()->prepare($query);
-$stmt->execute(array(":an"=>$account_number, ":id"=>$_SESSION["id"]));
-//check your DB, account should be created successfuly 
+
 }
 if(isset($_POST["created"])) {
     $name = "";
@@ -44,24 +30,56 @@ if(isset($_POST["created"])) {
         die();//terminates the rest of the script
     }
     try {
-        require("common.inc.php");
-        $query = file_get_contents(__DIR__ . "/queries/Insert_table_Accounts.sql");
+require("common.inc.php");
+//find the max id in the table
+$query = "SELECT MAX(id) as max from Accounts";
+$stmt = getDB()->prepare($query);
+$stmt->execute();
+$r = $stmt->fetch(PDO::FETCH_ASSOC);
+$max = (int)$r["max"];//should really check that this value is given correctly, I'm unsafely using it
+$max += 1;//increment by 1 (since this should be the new id that'll get automatically generated
+//pad the number with 0s to the left (this will fit the requirement and be unique since it's based on id
+$account_number = str_pad($str,12,"0",STR_PAD_LEFT);//read it https://www.w3schools.com/php/func_string_str_pad.asp
+//insert the new account number and associate it with the logged in user
+$query = "INSERT INTO Accounts(account_number, id, name) VALUES(:an, :id, :name)";
+$stmt = getDB()->prepare($query);
+$stmt->execute(array(":an"=>$account_number, ":id"=>$_SESSION["id"], ":name"=>$name));
+$worldAcct = -1;
+//TODO fetch world account from DB so we can get the ID, I defaulted to -1 so you implement this portion. Do not hard code the value here.
+//$worldAcct = $result["id"];
+//end fetch world account id
+
+        $query = "INSERT INTO Transactions(acct_id_src, acct_id_dest, change, type) VALUES (:src, :dest, :change, :type)";
+       
         if(isset($query) && !empty($query)) {
             $stmt = getDB()->prepare($query);
+//part 1
+$balance *= -1;//flip
             $result = $stmt->execute(array(
-                ":name" => $name,
-                ":balance" => $balance
+                ":src" => $worldAcct,
+                ":dest" => $max, //<- should really get the last insert ID from the account query, but $max "should" be accurate
+":change"=>$balance,
+":type"=>"deposit", //or it can be "create" or "new" if you want to distinguish between deposit and opening an account
             ));
-            $e = $stmt->errorInfo();
-            if ($e[0] != "00000") {
-                echo var_export($e, true);
-            } else {
-                if ($result) {
-                    echo "Successfully inserted new account: " . $name;
-                } else {
-                    echo "Error inserting record";
-                }
-            }
+//part 2
+$balance *= -1;//flip
+            $result = $stmt->execute(array(
+                ":src" => $max,
+                ":dest" => $worldAcct, //<- should really get the last insert ID from the account query, but $max "should" be accurate
+":change"=>$balance,
+":type"=>"deposit", //or it can be "create" or "new" if you want to distinguish between deposit and opening an account
+            ));
+//get new balance
+$query = "SELECT SUM(change) as balance from Transactions where acct_src_id = :acct";
+$stmt = getDB()->prepare($query);
+$stmt->execute(":id"=>$max);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+//TODO, should properly check to see if we have data and all
+$sum = (int)$result["balance"];
+//update balance
+$query = "UPDATE Accounts set balance = :bal where id = :id";
+$stmt = getDB()->prepare($query);
+$stmt->execute(":bal"=>$sum, ":id"=>$max);
         }
         else{
             echo "Failed to find Insert_table_Accounts.sql file";
