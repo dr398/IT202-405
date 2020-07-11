4232743d8f31bb8c169828df799e905be56fdd8e
dr398 <65628616+dr398@users.noreply.github.com>
2020-07-11 13:21:51 -0400

    Update create.php

--- public_html/Milestone2/create.php
+++ public_html/Milestone2/create.php
@@ -77,7 +77,6 @@ $balance *= -1;//flip
 $query = "SELECT SUM(change) as balance from Transactions where acct_src_id = :acct";
 $stmt = getDB()->prepare($query);
 $stmt->execute([":id"=>$max]);
-echo var_export($stmt->errorInfo(), true);
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 //TODO, should properly check to see if we have data and all
 $sum = (int)$result["balance"];
