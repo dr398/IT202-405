81aceba156140be7230afd1559ce445f1a0b5339
dr398 <65628616+dr398@users.noreply.github.com>
2020-07-29 14:19:58 -0400

    Update AccountsTable.sql

--- public_html/Milestone2/AccountsTable.sql
+++ public_html/Milestone2/AccountsTable.sql
@@ -2,7 +2,6 @@ CREATE TABLE Accounts (
     id int auto_increment,
     name varchar(20) unique,
     balance decimal(12,2) default 0.00,
-    is_active tinyint(1),
     account_number varchar(12) not null;
     created datetime default current_timestamp,
     modified datetime default current_timestamp on update current_timestamp,
