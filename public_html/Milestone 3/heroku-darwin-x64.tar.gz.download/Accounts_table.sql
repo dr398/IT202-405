a6c7fe0a3e80b4937c30bd36c36f2754675ba014
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-03 12:27:42 -0400

    Create Accounts_table.sql

---
+++ public_html/Milestone 3/heroku-darwin-x64.tar.gz.download/Accounts_table.sql
@@ -0,0 +1,10 @@
+  
+CREATE TABLE Accounts (
+    id int auto_increment,
+    name varchar(20) unique,
+    balance decimal(12,2) default 0.00,
+    account_number varchar(12) not null;
+    created datetime default current_timestamp,
+    modified datetime default current_timestamp on update current_timestamp,
+    primary key (id)
+)
