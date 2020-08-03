8b0991503c02a7e9a8086a995ddc22ab5ed8d926
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-03 12:37:22 -0400

    Create header.php

---
+++ public_html/Milestone 3/header.php
@@ -0,0 +1,30 @@
+<head>
+    <title>Bank Application</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<?php
+require("common.inc.php");
+session_start();
+?>
+<nav>
+    <ul>
+        <li>
+            <a href="home.php">Home</a>
+        </li>
+        <li>
+            <a href="login.php">Login</a>
+        </li>
+        <li>
+            <a href="register.php">Register</a>
+        </li>
+        <li>
+            <a href="create.php">Create</a>
+        </li>
+         <li>
+            <a href="edit.php">Edit</a>
+        </li>
+        <li>
+            <a href="logout.php">Logout</a>
+        </li>
+    </ul>
+</nav>
