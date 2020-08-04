62e13203161de7ec07ba68f81764ee35021c3370
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-04 16:32:28 -0400

    Update footerinfo.php

--- public_html/Milestone 3/footerinfo.php
+++ public_html/Milestone 3/footerinfo.php
@@ -1,6 +1,6 @@
   
 <footer style="padding-top: 200px; text-align: center; margin: 5px; padding-bottom: 15px;">
     <div class="footer"></div>
-    IT202 Copyright National Bank of America 2020_ <a href="contact.php">Contact Us_</a> <a href="bankingsafety.php">Online Banking Safety_</a>
+    IT202SUMMER2020 National Bank of America_ <a href="contact.php">Contact Us_</a> <a href="bankingsafety.php">Online Banking Safety_</a>
 
 </footer>
