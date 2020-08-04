99819bcaf609f231e189730db3cdfad5bad9e50d
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-04 17:03:36 -0400

    Update contact.php

--- public_html/Milestone 3/contact.php
+++ public_html/Milestone 3/contact.php
@@ -15,27 +15,27 @@
 </head>
 
 <div>
-    <h2 style="text-align:center;color:black; padding-bottom: 20px">Contact Us or find us on social media</h2>
+    <h2 style="text-align:center;color:black; padding-bottom: 20px">Contact us or follow our social media</h2>
 
         <p style="text-align: center"><span class="heading"><u>New York Divison</u></p>
         <p style="text-align: center"><span class="heading">Address: </span>49 St, New York, NY</p>
         <p style="text-align: center"><span class="heading">Phone: </span>632-111-3845</p>
         <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>nydivison@nboa.com</p>
 
-        <h3 style="color:darkslategrey; text-align: center;"><u>Boston Divison</u></h3>
+        <p style="text-align: center"><span class="heading"><u>Boston Divison</u></p>
         <p style="text-align: center"><span class="heading">Address: </span>2 Market Ave,Boston, Massachusetts</p>
         <p style="text-align: center"><span class="heading">Phone: </span>823-294-0011</p>
         <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>bostondivison@nboa.com</p>
 
-        <h3 style="color:darkslategrey; text-align: center;"><u>Nashville Divison</u></h3>
+        <p style="text-align: center"><span class="heading"><u>Nashville Divison</u></p>
         <p style="text-align: center"><span class="heading">Address: </span>23 Greg Blvd, Nashville,Tennessee</p>
         <p style="text-align: center"><span class="heading">Phone: </span>222-194-9345</p>
         <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>nashvilledivison@nboa.com</p>
     
         <h3 style="color:darkslategrey; text-align: center;"><u>Social Media</u></h3>
-        <p style="text-align: center"><span class="heading">Facebook: </span>https://www.facebook.com/NBOA/</p>
-        <p style="text-align: center"><span class="heading">Instagram: </span>https://www.instagram.com/NBOA/</p>
-           <p style="text-align: center"><span class="heading">Twitter: </span>https://www.twitter.com/NBOA/</p>
+        <p style="text-align: center"><span class="heading">Facebook: </span>https://www.facebook.com/nboa/</p>
+        <p style="text-align: center"><span class="heading">Instagram: </span>https://www.instagram.com/nboa/</p>
+          <p style="text-align: center"><span class="heading">Twitter: </span>https://www.twitter.com/nboa/</p>
         
 
 </div>
