064730385a67545a44bcef5e444b381a5bebdc4a
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-04 10:32:36 -0400

    Create contact.php

---
+++ public_html/Milestone 3/contact.php
@@ -0,0 +1,37 @@
+<?php include ("header.php") ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .heading{
+            font-weight:bold;
+            color:black;
+        }
+    </style>
+
+</head>
+
+<div>
+    <h2 style="text-align:center;color:black; padding-bottom: 20px">Contact Us</h2>
+
+        <h3 style="color:darkslategrey; text-align: center;"><u>India Branch</u></h3>
+        <p style="text-align: center"><span class="heading">Address: </span>PSB, Maharashtra, Mumbai</p>
+        <p style="text-align: center"><span class="heading">Phone: </span>+91 9123 4567</p>
+        <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>psbbranch.in@psb.co</p>
+
+        <h3 style="color:darkslategrey; text-align: center;"><u>Australia Branch</u></h3>
+        <p style="text-align: center"><span class="heading">Address: </span>PSB, Sydney, Australia</p>
+        <p style="text-align: center"><span class="heading">Phone: </span>+61 0245 6789</p>
+        <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>psbbranch.au@psb.co</p>
+
+        <h3 style="color:darkslategrey; text-align: center;"><u>Canada Branch</u></h3>
+        <p style="text-align: center"><span class="heading">Address: </span>PSB, Ontario, Canada</p>
+        <p style="text-align: center"><span class="heading">Phone: </span>+1 226 1234</p>
+        <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>psbbranch.ca@psb.co</p>
+
+</div>
+
+<?php include 'footer.php';?>
