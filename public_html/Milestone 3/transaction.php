608ff179332684644bc48ff71ccca21a353b4b52
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-04 10:37:42 -0400

    Create transaction.php

---
+++ public_html/Milestone 3/transaction.php
@@ -0,0 +1,21 @@
+<?php
+include ("header.php");
+?>
+
+<h2>Transactions</h2>
+
+    <ul>
+        <li>
+            <a href="deposit.php">Make a Deposit</a>
+        </li>
+        <li>
+            <a href="withdraw.php">Withdraw Money</a>
+        </li>
+        <li>
+            <a href="transfer.php">Transfer Money</a>
+        </li>
+    
+    </ul>
+
+
+<?php include 'footerinfo.php';?>
