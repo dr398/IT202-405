801c2451e529a1d36d77d967d81bcf6453f942f6
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-04 16:24:42 -0400

    Update footerinfo.php

--- public_html/Milestone 3/footerinfo.php
+++ public_html/Milestone 3/footerinfo.php
@@ -1,6 +1,6 @@
   
 <footer style="padding-top: 200px; text-align: center; margin: 5px; padding-bottom: 15px;">
     <div class="footer"></div>
-    IT202 Copyright 2020 -- <a href="contact.php">Contact --</a> <a href="bankingtips.php">Online Banking Safety --</a>
+    IT202 Copyright 2020-- <a href="contact.php">Contact Us--</a> <a href="bankingtips.php">Online Banking Safety--</a>
 
 </footer>
