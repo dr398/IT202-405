4275a61144d810ea9e811eed2a38346dac5d91bd
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-04 16:24:25 -0400

    Update footerinfo.php

--- public_html/Milestone 3/footerinfo.php
+++ public_html/Milestone 3/footerinfo.php
@@ -1,6 +1,6 @@
   
 <footer style="padding-top: 200px; text-align: center; margin: 5px; padding-bottom: 15px;">
     <div class="footer"></div>
-    Copyright@PSB 2020_____ <a href="contact.php">Contact_____</a> <a href="bankingtips.php">Online Banking Tips_____</a>
+    IT202 Copyright 2020 -- <a href="contact.php">Contact --</a> <a href="bankingtips.php">Online Banking Safety --</a>
 
 </footer>
