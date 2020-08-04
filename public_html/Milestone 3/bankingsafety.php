e832ce7ae9f9080548c09ed792a22f26bd943ca4
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-04 17:22:59 -0400

    Update bankingsafety.php

--- public_html/Milestone 3/bankingsafety.php
+++ public_html/Milestone 3/bankingsafety.php
@@ -1,16 +1,16 @@
 <?php include ("header.php")
 ?>
 
-    <h2 style="text-align:center;color:darkslategrey;">Online Banking Safety</h2>
+    <h2 style="text-align:center;color:darkslategrey;">Top 5 ways to be safe while online banking</h2>
 
     <div class="contact">
 
         <ul style="padding-top: 20px; font-family: Arial;">
-            <li>Phishing is a fraudulent attempt, usually made through email, phone calls, SMS (or other) seeking confidential information.</li>
-            <li>PSB Bank or any of its representatives never send emails or texts to get your personal information, communication is always over phone or in office.</li>
-            <li>Any unknown activity that may occur should be reported immediately. </li>
-            <li>Change your account password periodically to maintain security.</li>
-            <li>Keep in check your last log-in date and time by using the profile page.</li>
+            <li>Please do not use public WiFi for private banking.</li>
+            <li>Change your password frequently so your account can not be unaccessable by strangers. </li>
+            <li>Stay alert with our banking notifications to be up to date with our system. </li>
+            <li>Know your transactions, keep track with your deposits, withdrawls, and trasnfers.</li>
+            <li>If you see any suspicious activity in your account please contact us immediately.</li>
         </ul>
 
 <?php include 'footerinfo.php';?>
