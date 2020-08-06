238a2dbf482a7ae5b55d30585c49370d8686d2fc
dr398 <65628616+dr398@users.noreply.github.com>
2020-08-06 19:25:36 -0400

    Update delete.php

--- public_html/Milestone 3/delete.php
+++ public_html/Milestone 3/delete.php
@@ -5,7 +5,7 @@ if (isset($_GET["accountId"]) && !empty($_GET["accountId"])){
         $accountId = (int)$_GET["accountId"];
         $query = file_get_contents(__DIR__ . "/Delete_one_table_Accounts.sql");
         if(isset($query) && !empty($query)) {
-            require("common.inc.php");
+           
             $stmt = getDB()->prepare($query);
             $stmt->execute([":id"=>$accountId]);
             $e = $stmt->errorInfo();
